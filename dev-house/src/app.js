b9c144d794c6fd908ea887bea286f19adf30de71
lucassantos22 <lucas.afsantos@hotmail.com>
2022-10-26 23:21:42 -0300

    chore: add mongoose dependendy

--- dev-house/src/app.js
+++ dev-house/src/app.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 import routes from './routes';
 
 const app = express();
+
+mongoose.connect('mongodb+srv://devhouse:devhouse@devhouse.kqf7nd7.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 app.use(express.json())
 app.use(routes)
