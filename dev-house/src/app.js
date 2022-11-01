393f7564f67fc4800d81532edc6392f55acf3c22
lucassantos22 <lucas.afsantos@hotmail.com>
2022-11-01 15:57:22 -0300

    chore: add static routes to images

--- dev-house/src/app.js
+++ dev-house/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path'
 
 import routes from './routes';
 
@@ -10,7 +11,13 @@ mongoose.connect('mongodb+srv://devhouse:devhouse@devhouse.kqf7nd7.mongodb.net/?
     useUnifiedTopology: true,
 })
 
+// Cadastrar rotas para imagens dentro de uploads
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+// Configuração para aceitar json no body da requisição
 app.use(express.json())
+
+// Importando nossas rotas
 app.use(routes)
 
 export default app
--- dev-house/src/controllers/HouseController.js
+++ dev-house/src/controllers/HouseController.js
@@ -2,9 +2,19 @@ import House from "../models/House"
 
 class HouseController {
     async store(req, res) {
-        console.log(req.body)
-        console.log(req.file)
-        res.json({ OK: true })
+        const { filename } = req.file
+        const { description, price, location, status } = req.body
+        const { user_id } = req.headers
+
+        const house = await House.create({
+            thumbnail: filename,
+            description,
+            price,
+            location,
+            status,
+            user: user_id
+        })
+        res.status(201).json(house)
     }
 }
 
