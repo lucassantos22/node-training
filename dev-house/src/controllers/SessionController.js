b04d7ce5d1b5cbaea27cf2c3b6e9012360b5de4d
lucassantos22 <lucas.afsantos@hotmail.com>
2022-11-08 20:52:33 -0300

    chore: add session validation

--- dev-house/src/controllers/SessionController.js
+++ dev-house/src/controllers/SessionController.js
@@ -1,8 +1,19 @@
+import * as Yup from "yup";
+
 import User from "../models/User";
 
 class SessionController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required()
+        })
+
         const { email } = req.body;
+
+        if (!await schema.isValid(req.body)) {
+            return res.status(400).json({ message: 'Invalid email' });
+        }
+
         const userExists = await User.findOne({ email });
         if (userExists) return res.status(400).json({ message: 'User already exists' })
         const user = await User.create({
