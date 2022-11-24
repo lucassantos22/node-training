0bd4e929ffc14120ea7a9465fdfdf948859a853a
lucassantos22 <lucas.afsantos@hotmail.com>
2022-11-24 00:36:06 -0300

    chore: add yup validation at User Controller

--- tasklist/src/app/controllers/UserController.js
+++ tasklist/src/app/controllers/UserController.js
@@ -1,7 +1,19 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().required().min(6),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
         const { body } = req;
         const userExists = await User.findOne({
             where: { email: body.email },
@@ -18,6 +30,17 @@ class UserController {
     }
 
     async update(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string().email(),
+            oldPassword: Yup.string().min(6),
+            password: Yup.string().min(6),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Incorrect data' });
+        }
+
         const { email, oldPassword } = req.body;
         const user = await User.findByPk(req.userId);
         if (email !== user.email) {
