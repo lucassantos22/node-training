d78eddbc28ff541f4dd25d7ff46c50d258f57af3
lucassantos22 <lucas.afsantos@hotmail.com>
2022-11-30 00:52:20 -0300

    chore: add task controller

--- tasklist/src/routes.js
+++ tasklist/src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import TaskController from './app/controllers/TaskController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,5 +14,7 @@ routes.post('/sessions', SessionController.store);
 // Todas as rotas abaixo desse routes.use necessitarão de autenticação
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.get('/tasks', TaskController.index);
+routes.post('/tasks', TaskController.store);
 
 export default routes;
